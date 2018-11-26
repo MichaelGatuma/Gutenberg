5d3b1effbbf2c1a06a7f004da1fa7ebe21bc047a
StevenShyaka <sshya024@uottawa.ca>
2018-11-25 19:56:43 -0500

    Fixed the TypeError Bug

--- search.py
+++ search.py
@@ -3,34 +3,30 @@ import requests
 import urllib
 from bs4 import BeautifulSoup
 
-def url_scrape(links):
+def url_scrape():
     for item in soup.find_all('h3', attrs={'class' : 'r'}):
-        links.append(item.a['href'].split('&sa')[0].split('/url?q=')[1])
+        try:
+            print(item.a['href'].split('/url?q=')[1].split('&sa', 1)[0])
+        except TypeError:
+            pass
 
-def paginate(pages):
+def paginate():
     for page in soup.find_all('table', attrs={'id' : 'nav'}):
         for number in page.find_all('a'):
             if (number['href'].split('start=')[1].split('&sa')[0]) not in pages:
                 pages.append(number['href'].split('start=')[1].split('&sa')[0])
 
-query = 'wajdnawdliwunilnuid'
+query = 'lag'
 query = urllib.quote_plus(query)
 
 r = requests.get('https://www.google.com/search?q={}'.format(query))
-soup = BeautifulSoup(r.content, "html.parser")
+soup = BeautifulSoup(r.content, 'html.parser')
 
 i = 0
 
 pages = []
-paginate(pages)
+paginate()
+print(pages)
 
 links = []
-url_scrape(links)
-
-try:
-    while i < pages[-1]:
-        
-
-except IndexError:
-    pass
-
+url_scrape()
